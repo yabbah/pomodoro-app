96541c46205a7e39efcf0c5fdaa4e7d2bec16831
yabbah <elmeholt@gmail.com>
2021-03-09 12:53:43 +0100

    timer func

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from tkinter import *
+import math
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -9,10 +11,54 @@ WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 
+
+def reset_clicked():
+    print("Reset")
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 
+
+def start_clicked():
+    count_down(5 * 60)
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 
+
+def count_down(count):
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+    canvas.itemconfig(canvas_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pompdoro Application")
+window.config(padx=100, pady=50, bg=YELLOW)
+timer_text = Label(text="Timer", font=(FONT_NAME, 50), fg=GREEN, bg=YELLOW)
+timer_text.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+canvas.grid(column=1, row=1)
+
+canvas_text = canvas.create_text(
+    100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+
+
+start_button = Button(text="Start", command=start_clicked,
+                      highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text="Reset", command=reset_clicked,
+                      highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+checkmark_text = Label(text="✓", font=(FONT_NAME, 24), fg=GREEN, bg=YELLOW)
+checkmark_text.grid(column=1, row=3)
+
+window.mainloop()
